a48ffc0682c9e06262b006ef1cc40ddd0ce88e02
mrralphpierre <mrralphpierre@gmail.com>
2021-11-17 21:31:59 -0500

    added pages into components

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,14 @@ import Intro from "./components/intro/Intro";
 import ProductList from "./components/productList/ProductList";
 import Toggle from "./components/toggle/Toggle";
 import { ThemeContext } from "./context";
+import React from "react";
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Routes,
+  Link
+} from "react-router-dom";
 
 const App = () => {
   const theme = useContext(ThemeContext);
@@ -16,11 +24,45 @@ const App = () => {
         color: darkMode && "white",
       }}
     >
-      <Toggle />
-      <Intro />
-      <About />
-      <ProductList />
-      <Contact />
+      
+      <BrowserRouter>
+
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            
+
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Nav!
+        </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                {/* <Link className="dropdown-item" to="/contact">Contact</Link> */}
+                <Link class="dropdown-item" to="/"> Intro </Link>
+                <Link class="dropdown-item" to="/about"> About </Link>
+                <Link class="dropdown-item" to="/ProductList"> Product List </Link>
+                <Link class="dropdown-item" to="/contact"> Contact </Link>
+              </div>
+            </li>
+
+
+            
+          </ul>
+        </div>
+      </nav>
+        <Routes>
+          <Route path="/" element={<Intro />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/ProductList" element={<ProductList />} />
+          <Route path="/Contact" element={<Contact />} />
+
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
