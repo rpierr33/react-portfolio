5372b475faa136b5933f40ffbe23ae2729595710
mrralphpierre <mrralphpierre@gmail.com>
2021-12-22 10:42:17 -0500

    removed darktheme from contact page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,15 +2,15 @@ import "./contact.css";
 import Phone from "../../img/phone.png";
 import Email from "../../img/email.png";
 import Address from "../../img/address.png";
-import { useContext, useRef, useState } from "react";
+import {  useRef, useState } from "react";
 import emailjs from "emailjs-com";
-import { ThemeContext } from "../../context";
+
 
 const Contact = () => {
     const formRef = useRef();
     const [done, setDone] = useState(false)
-    const theme = useContext(ThemeContext);
-    const darkMode = theme.state.darkMode;
+    // const theme = useContext(ThemeContext);
+    // const darkMode = theme.state.darkMode;
   
     const handleSubmit = (e) => {
       console.log("this function works");
@@ -60,14 +60,14 @@ const Contact = () => {
               <b>What do you have in mind?</b> Never hesitate to reach out. Always available for
               freelancing if the right project comes along. 
             </p>
-            {/* <form ref={formRef} onSubmit={handleSubmit}>
-              <input style={{backgroundColor: darkMode && "#333"}} type="text" placeholder="Name" name="user_name" />
+            <form ref={formRef} onSubmit={handleSubmit}>
+              {/* <input style={{backgroundColor: darkMode && "#333"}} type="text" placeholder="Name" name="user_name" />
               <input style={{backgroundColor: darkMode && "#333"}} type="text" placeholder="Subject" name="user_subject" />
               <input style={{backgroundColor: darkMode && "#333"}} type="text" placeholder="Email" name="user_email" />
               <textarea style={{backgroundColor: darkMode && "#333"}} rows="5" placeholder="Message" name="message" />
-              <button>Submit</button>
+              <button>Submit</button> */}
               {done && "Thank you..."}
-            </form> */}
+            </form>
           </div>
         </div>
       </div>
